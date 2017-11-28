c09bf940605108232a216f29235b0f7ea731166e
sihamza <hamzus007jouini@gmail.com>
2017-11-28 12:53:37 +0100

    Add files via upload

--- ex4.c
+++ ex4.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 void inv_maj ( char *ch) ;
 int main()
-{   char ch[100];
+{
+    char ch[100];
     printf("Donner une chaine ");
     scanf("%s",&ch);
     inv_maj(ch);
@@ -11,13 +12,20 @@ int main()
 
 
 void inv_maj ( char *ch )
-{ int i ;
-  char x[100];
-  for ( i = 0 ; i <strlen(ch) ; i ++ )
-    {if (ch[i]>94)
-    x[i]=ch[i]-32;
-    else
-    x[i]=ch[i]+32;}
-    *ch= x ;
+{
+    int i ;
+    char x[100];
 
-}
+    for ( i = 0 ; i <strlen(ch) ; i ++ )
+    {
+        if (ch[i]>='A')
+            x[i]=ch[i]-('A' -'a');
+        else
+            if (ch[i]>='a')
+            x[i]=ch[i]+('A' -'a');
+
+    }
+    x[i+1]= '\0' ;
+    ch = x ;}
+
+
