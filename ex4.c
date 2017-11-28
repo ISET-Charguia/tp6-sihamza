50f2b576188cd64910315b75efd6a798d5829c27
sihamza <hamzus007jouini@gmail.com>
2017-11-28 13:22:52 +0100

    Add files via upload

--- ex4.c
+++ ex4.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-void inv_maj ( char *ch) ;
-int main()
+#include <string.h>
+void inv_maj ( char ch[] )
 {
-    char ch[100];
+    int i=0 ;
+
+
+   while (ch[i]!='\0')
+    {
+       if ((ch[i]>='A')&&(ch[i]<='Z')){
+        ch[i]=ch[i]+32;}
+        else
+           if ((ch[i]>='a')&&(ch[i]<='z')){
+
+
+            ch[i]=ch[i]-32;}
+i++;
+    }
+puts (ch);
+    ;}
+    int main()
+
+{
+    char *ch[100];
     printf("Donner une chaine ");
     scanf("%s",&ch);
+    printf("le resultat = ");
     inv_maj(ch);
-    printf("le resultat = %s",ch);
 }
 
 
-void inv_maj ( char *ch )
-{
-    int i ;
-    char x[100];
-
-    for ( i = 0 ; i <strlen(ch) ; i ++ )
-    {
-        if (ch[i]>='A')
-            x[i]=ch[i]-('A' -'a');
-        else
-            if (ch[i]>='a')
-            x[i]=ch[i]+('A' -'a');
-
-    }
-    x[i+1]= '\0' ;
-    ch = x ;}
 
 
